ee6f28c946e3b0ee8d141487def57df2f0c41822
DESKTOP-BSF4VEL <daikadai1026@gmail.com>
2020-04-20 18:52:17 +0700

    2. Initial List, UseState Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import './App.css';
 import Alert from './components/Alert';
 import ExpenseForm from './components/ExpenseForm';
 import ExpenseList from './components/ExpenseList';
+import uuid from 'uuid/v4';
+
+
+const initialExpenses = [
+  { id: uuid(), charge: 'rent', amount: 1600 },
+  { id: uuid(), charge: 'car payment', amount: 400 },
+  { id: uuid(), charge: 'credit card bill', amount: 1200 }
+]
 
 function App() {
+  const [expenses, setExpenses] = useState(initialExpenses);
+
   return (
     <Fragment>
       <Alert />
